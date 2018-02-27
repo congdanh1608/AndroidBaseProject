29967b7eb6da10ab429f7d34c34d1fecce324b83
CongDanh <congdanh1608@gmail.com>
2018-02-27 21:13:31 +0800

    Add module to appcomponent

--- app/src/main/java/com/congdanh/androidbaseproject/di/component/AppComponent.java
+++ app/src/main/java/com/congdanh/androidbaseproject/di/component/AppComponent.java
@@ -2,7 +2,9 @@ package com.congdanh.androidbaseproject.di.component;
 
 import com.congdanh.androidbaseproject.MyApplication;
 import com.congdanh.androidbaseproject.di.module.AppModule;
+import com.congdanh.androidbaseproject.di.module.NetworkModule;
 import com.congdanh.androidbaseproject.di.module.RoomModule;
+import com.congdanh.androidbaseproject.di.module.ServiceManager;
 import com.congdanh.androidbaseproject.di.module.ShareprefsModule;
 
 import javax.inject.Singleton;
@@ -13,7 +15,7 @@ import dagger.Component;
  * Created by congdanh on 2/25/2018.
  */
 @Singleton
-@Component(modules = {AppModule.class, ShareprefsModule.class})
+@Component(modules = {AppModule.class, NetworkModule.class, ServiceManager.class, ShareprefsModule.class})
 public interface AppComponent {
     void inject(MyApplication myApplication);
 
