50c4dacb289699e9f5de5fdc844b6bd427beda32
DanhTC <congdanh1608@gmail.com>
2018-07-30 00:34:23 +0700

    update glide and fix bug crash of glide

--- app/src/main/java/com/congdanh/androidbaseproject/view/activity/main/MainActivityPresenter.java
+++ app/src/main/java/com/congdanh/androidbaseproject/view/activity/main/MainActivityPresenter.java
@@ -36,4 +36,8 @@ public class MainActivityPresenter {
             }
         };
     }
+
+    public String getImage(){
+        return "https://i.amz.mshcdn.com/FZXQWbpZ9hwycnowQwjI7zjejxk=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F528403%2F153ae5f3-17fa-4620-b8b1-33cd0d354385.jpg";
+    }
 }
