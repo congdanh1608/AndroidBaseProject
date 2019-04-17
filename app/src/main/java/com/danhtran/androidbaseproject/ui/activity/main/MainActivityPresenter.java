b699771ab52c102ff7d8024e114330dfa32413f2
DanhTC <congdanh1608@gmail.com>
2019-04-17 14:38:43 +0700

    Add transition animation for fragment

--- app/src/main/java/com/danhtran/androidbaseproject/ui/activity/main/MainActivityPresenter.java
+++ app/src/main/java/com/danhtran/androidbaseproject/ui/activity/main/MainActivityPresenter.java
@@ -1,10 +1,6 @@
 package com.danhtran.androidbaseproject.ui.activity.main;
 
-import android.content.Intent;
 import android.view.View;
-
-import com.danhtran.androidbaseproject.ui.activity.demo_database.DemoActivity;
-import com.danhtran.androidbaseproject.ui.activity.demo_permission.Demo1Activity;
 
 /**
  * Created by danhtran on 2/25/2018.
@@ -21,23 +17,12 @@ public class MainActivityPresenter {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Class activity = null;
-                switch (index) {
-                    case 0:
-                    default:
-                        activity = DemoActivity.class;
-                        break;
-                    case 1:
-                        activity = Demo1Activity.class;
-                        break;
-                }
-                Intent intent = new Intent(mainActivity, activity);
-                mainActivity.startActivity(intent);
+                
             }
         };
     }
 
-    public String getImage(){
+    public String getImage() {
         return "https://i.amz.mshcdn.com/FZXQWbpZ9hwycnowQwjI7zjejxk=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F528403%2F153ae5f3-17fa-4620-b8b1-33cd0d354385.jpg";
     }
 }
